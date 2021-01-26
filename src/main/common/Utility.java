5a00c0fda9fda33ca47d29d3b4e0b7369fe08a8a
Arthur Arnx <arthur.arnx@gmail.com>
2021-01-26 18:52:01 +0100

    write bytes

--- src/main/common/Utility.java
+++ src/main/common/Utility.java
@@ -20,6 +20,19 @@ public class Utility {
 		}
 	}
 	
+	public static void writeBytes(byte[] bytes, DataOutputStream outputStream) {
+		int size = bytes.length;
+		try {
+			outputStream.writeInt(size);
+			for (int i=0; i<size; i++) {
+				outputStream.writeByte(bytes[i]);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public static String readString(DataInputStream inputStream) {
 		byte[] bytesText = null;
 		try {
@@ -33,5 +46,21 @@ public class Utility {
 			e.printStackTrace();
 		}
 		return new String(bytesText);
+	}
+
+
+	public static byte[] readBytes(DataInputStream inputStream) {
+		byte[] bytesText = null;
+		try {
+			int size = inputStream.readInt();
+			bytesText = new byte[size];
+			for(int i=0; i<size; i++) {
+				bytesText[i] = inputStream.readByte();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return bytesText;
 	}
 }
